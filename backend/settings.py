0d25913676b6f14bff55c74312c25c55f42c7b97
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-12-07 06:11:18 -0800

    fix: allowed hosts updated

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY", config("SECRET_KEY"))
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
 ALLOWED_HOSTS = [
-    'the-eco-platform.onrender.com'
+    'the-eco-platform.onrender.com',
+    '127.0.0.1'
+    'fortheeco.com'
 ]
 
 
