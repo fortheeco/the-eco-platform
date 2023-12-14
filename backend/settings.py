d01f2673dd64a4c5eb255d23e99c4b5574a9a9aa
Neon-jeff <omaksteejay@gmail.com>
2023-12-14 21:09:55 +0100

    DB MIGRATIONS AND SOME PAGE FIXES

--- backend/settings.py
+++ backend/settings.py
@@ -95,17 +95,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# if not DEBUG:
-DATABASES = {
+if not DEBUG:
+   DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
+   }
+else:
+   DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-# else:
-#  DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
