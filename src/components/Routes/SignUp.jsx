64645d5ffcf91ab6a937ac165fc46fe86858f24f
SageKyle <ominyipaulo@gmail.com>
2024-05-21 01:06:25 +0100

    complete markup for intro (Signup)

--- src/components/Routes/SignUp.jsx
+++ src/components/Routes/SignUp.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { styles } from "../../style";
+import Intro from "../Signup/Intro";
 
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return <div  className={`${styles.padding} ${styles.flexBetween} items-center mx-auto w-full selection:bg-ecoGreen`}>
+    <Intro />
+  </div>;
 };
